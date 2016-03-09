2de584d18fba6f69291663ed5907b84a05ba6a86
Simon Rdding <s1m0n.r3dd1ng@gmail.com>
2016-03-09 19:22:34 +0000

    Add order to bottom of book working.

--- test_py_lend.py
+++ test_py_lend.py
@@ -19,6 +19,24 @@ class TestBook(unittest.TestCase):
     self.assertFalse(book.borrows)
     self.assertEqual([l], book.lends)
 
+  def test_add_borrow_to_bottom_of_bool(self):
+    book = Book(5)
+    b1 = Order(None, Side.BORROW, 10000, 5, 7)
+    b2 = Order(None, Side.BORROW, 10000, 5, 6)
+    book.add(b1)
+    book.add(b2)
+    self.assertFalse(book.lends)
+    self.assertEqual([b1, b2], book.borrows)
+
+  def test_add_lend_to_bottom_of_bool(self):
+    book = Book(5)
+    l1 = Order(None, Side.LEND, 10000, 5, 7)
+    l2 = Order(None, Side.LEND, 10000, 5, 8)
+    book.add(l1)
+    book.add(l2)
+    self.assertFalse(book.borrows)
+    self.assertEqual([l1, l2], book.lends)
+
 
 if __name__ == '__main__':
  unittest.main() 
